41afd117aaa965d9bd614b90d978921fed333101
Sofia <sophia.shashova@yandex.ru>
2024-06-10 14:34:43 +0400

    Add gendiff to proj

---
+++ Makefile
@@ -0,0 +1,8 @@
+install:
+		npm ci
+gendiff -h: 
+		node bin/gendiff.js
+lint:
+		npx eslint .
+fix: 
+		npx eslint --fix .
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .version('1.0.0');
+
+  export default program;
