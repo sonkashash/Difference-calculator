e84c408b5f635bb055684055a58415bf974dbbcc
Sofia <sophia.shashova@yandex.ru>
2024-06-13 16:55:50 +0400

    Delete duplicates

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,8 +2,9 @@ import genDiff from '../src/index.js';
 
 test('genDiff', () => {
     const gendiff = JSON.parse(genDiff('file1.json', 'file2.json'));
-    expect(gendiff).toEqual({"- follow":false," host ":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true});
+    const result = {"- follow":false," host ":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true};
+    expect(gendiff).toEqual(result);
 
     const gendiff2 = JSON.parse(genDiff('/src/file1.json', '/src/file2.json'));
-    expect(gendiff2).toEqual({"- follow":false," host ":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true});
+    expect(gendiff2).toEqual(result);
 })
