f50b5eaf6f58be7411a1ee128094f2b48e94d948
Sofia <sophia.shashova@yandex.ru>
2024-06-28 17:28:38 +0400

    create index.js file

---
+++ __tests__/index.test.js
@@ -0,0 +1,38 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import genDiff from "../src/index.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const getFixturePath = (filename) =>
+  path.join(__dirname, "..", "__fixtures__", filename);
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), "utf-8");
+
+const diffStylish = JSON.stringify(readFile("diff_stylish.txt"));
+const diffPlain = JSON.stringify(readFile("diff_plain.txt"));
+const diffJson = JSON.stringify(readFile("diff_json.txt"));
+
+const testData = [
+  { format: "stylish", expected: diffStylish },
+  { format: "plain", expected: diffPlain },
+  { format: "json", expected: diffJson },
+];
+
+test.each(testData)("genDiff $format", ({ format, expected }) => {
+  expect(JSON.stringify(genDiff("file1.json", "file2.json", format))).toEqual(
+    expected
+  );
+});
+
+test.each(testData)("Output: $format", ({ format, expected }) => {
+  expect(JSON.stringify(genDiff("file1.yml", "file2.yml", format))).toEqual(
+    expected
+  );
+});
+
+test.each(testData)("Output: $format", ({ format, expected }) => {
+  expect(JSON.stringify(genDiff("file1.json", "file2.yml", format))).toEqual(
+    expected
+  );
+});
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import genDiff from '../src/genDiff.js';
+import { program } from "commander";
+import genDiff from "../src/index.js";
 
 program
-  .name('gendiff')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information')
-  .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .name("gendiff")
+  .argument("<filepath1>")
+  .argument("<filepath2>")
+  .description("Compares two configuration files and shows a difference.")
+  .helpOption("-h, --help", "output usage information")
+  .version("1.0.0")
+  .option("-f, --format <type>", "output format", "stylish")
   .action((filepath1, filepath2) => {
     const formatName = program.opts().format;
     const diff = genDiff(filepath1, filepath2, formatName);
