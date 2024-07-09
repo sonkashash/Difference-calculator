acdc02c88a24c0ff8483d73d11209f7b671f20d8
Sofia <sophia.shashova@yandex.ru>
2024-07-09 11:23:21 +0300

    Add fixtures from default path

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,9 @@
 import genDiff from '../src/index.js';
 import readFile from '../src/utils.js';
 
-const diffStylish = readFile('diff_stylish.txt');
-const diffPlain = readFile('diff_plain.txt');
-const diffJson = readFile('diff_json.txt');
+const diffStylish = readFile('__fixtures__/diff_stylish.txt');
+const diffPlain = readFile('__fixtures__/diff_plain.txt');
+const diffJson = readFile('__fixtures__/diff_json.txt');
 
 const testData = [
   { format: 'stylish', expected: diffStylish },
@@ -12,19 +12,19 @@ const testData = [
 ];
 
 test.each(testData)('genDiff $format', ({ format, expected }) => {
-  expect(genDiff('file1.json', 'file2.json', format)).toEqual(
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', format)).toEqual(
     expected,
   );
 });
 
 test.each(testData)('Output: $format', ({ format, expected }) => {
-  expect(genDiff('file1.yml', 'file2.yml', format)).toEqual(
+  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', format)).toEqual(
     expected,
   );
 });
 
 test.each(testData)('Output: $format', ({ format, expected }) => {
-  expect(genDiff('file1.json', 'file2.yml', format)).toEqual(
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.yml', format)).toEqual(
     expected,
   );
 });
--- src/utils.js
+++ src/utils.js
@@ -2,15 +2,14 @@ import process from 'process';
 import path from 'path';
 import fs from 'fs';
 
-const getAbsPath = (fileName) => {
+const getAbsPath = (pathToFile) => {
   const __dirname = process.cwd();
-  const absPath = path.resolve(__dirname, '__fixtures__', fileName);
+  const absPath = path.resolve(__dirname, pathToFile);
   return absPath;
 };
 
 const readFile = (pathToFile) => {
-  const fileName = pathToFile.split('/').at(-1);
-  const normalizedPath = getAbsPath(fileName);
+  const normalizedPath = pathToFile.startsWith('/') ? pathToFile : getAbsPath(pathToFile);
   return fs.readFileSync(normalizedPath, 'utf-8');
 };
 
