628cf6956395bb0f8e1ea6b670852b6eb403f34b
Sofia <sophia.shashova@yandex.ru>
2024-07-09 11:29:22 +0300

    Delete duplications from tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,6 +4,8 @@ import readFile from '../src/utils.js';
 const diffStylish = readFile('__fixtures__/diff_stylish.txt');
 const diffPlain = readFile('__fixtures__/diff_plain.txt');
 const diffJson = readFile('__fixtures__/diff_json.txt');
+const pathToFile1 = '__fixtures__/file1';
+const pathToFile2 = '__fixtures__/file2';
 
 const testData = [
   { format: 'stylish', expected: diffStylish },
@@ -12,19 +14,19 @@ const testData = [
 ];
 
 test.each(testData)('genDiff $format', ({ format, expected }) => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', format)).toEqual(
+  expect(genDiff(`${pathToFile1}.json`, `${pathToFile2}.json`, format)).toEqual(
     expected,
   );
 });
 
 test.each(testData)('Output: $format', ({ format, expected }) => {
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', format)).toEqual(
+  expect(genDiff(`${pathToFile1}.yml`, `${pathToFile2}.yml`, format)).toEqual(
     expected,
   );
 });
 
 test.each(testData)('Output: $format', ({ format, expected }) => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.yml', format)).toEqual(
+  expect(genDiff(`${pathToFile1}.json`, `${pathToFile2}.yml`, format)).toEqual(
     expected,
   );
 });
